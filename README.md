d9e9f785ab52fc0cd776d7d3b808529d6a8d6e90
rodgnathaniel <rodgnathaniel@gmail.com>
2019-05-02 09:59:06 -0700

    \

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# portfolio_sitev2
-remaking personal website. Goal is less abstract design and more features&lt;3
+# portfolio_site
---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse, JsonResponse
+
+# from .models import Game, Mode, State
+
+import json
+
+def home_view(request):
+
+    return render(request, "home/portfolio_home.html", {})
